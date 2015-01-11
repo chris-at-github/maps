e2cbe5b0bf7fb40b6fd5d7b2d1fc7cbbaaf86495
Christian Pschorr <pschorr.christian@gmail.com>
2015-01-11 14:18:34 +0100

    Berechnung der Größe des Tile-Containers auf Grund der Einstellungen zu den Kacheln

--- app/views/world/map/index.blade.php
+++ app/views/world/map/index.blade.php
@@ -1,7 +1,7 @@
 @include('layouts.header')
 
-	<div id="world-map-container">
-		<div class="tile-container">
+	<div id="world-map-container" class="map-container">
+		<div class="tile-container" style="width: {{$map->size->width}}px; height: {{$map->size->height}}px;">
 			@foreach($tiles as $tile)
 				<div class="tile" style="top: {{$tile->coordinates->y}}px; left: {{$tile->coordinates->x}}px;"></div>
 			@endforeach
