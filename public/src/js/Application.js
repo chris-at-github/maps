5c47478df4dc33f9af2b577834d24052a4a0f1df
Christian Pschorr <pschorr.christian@gmail.com>
2015-01-11 17:47:50 +0100

    Verarbeitung Tile per JavaScript

--- public/src/js/Application.js
+++ public/src/js/Application.js
@@ -1,4 +1,8 @@
-var Tile 				= require('./Tile.js');
+var Tile = require('./Tile.js');
+
+$('.tile-container > .tile').each(function(i, item) {
+	var tileInstance = new Tile(item);
+});
 
 // var tileContainer = $('#tile-container');
 // $.each(Maps.data.tiles, function(i, tileData) {
--- public/src/js/Tile.js
+++ public/src/js/Tile.js
@@ -1,12 +1,22 @@
 'use strict';
 
-function Tile(options) {
+function Tile(element, options) {
 	this.container 	= $('<div>');
 	this.options		= $.extend({}, Tile.DEFAULTS, options);
+	this.x					= 0;
+	this.y					= 0;
 
 	var instance 		= this;
 	var initialize 	= function() {
 		instance.container.addClass('tile');
+		instance.container
+			.on('mouseenter', $.proxy(instance.on, instance))
+			.on('mouseout', $.proxy(instance.off, instance))
+			.on('click', $.proxy(instance.activate, instance));
+	}
+
+	if(element !== undefined) {
+		this.container = $(element);
 	}
 
 	initialize();
@@ -28,6 +38,20 @@ Tile.prototype.render = function() {
 	});
 
 	return this.container;
+}
+
+Tile.prototype.on = function() {
+	this.container.addClass('on');
+}
+
+Tile.prototype.off = function() {
+	this.container.removeClass('on');
+}
+
+Tile.prototype.activate = function(e) {
+	var url = '/tile/' + this.x + '/' + this.y;
+
+	$.get(url);
 }
 
 module.exports = Tile;
