cc2e250e026c7ccb85737fdf4a72b5c4e42d4f92
Christian Pschorr <pschorr.christian@gmail.com>
2015-01-11 16:41:29 +0100

    Debug entfernt

--- app/controllers/WorldController.php
+++ app/controllers/WorldController.php
@@ -5,8 +5,6 @@ class WorldController extends ApplicationController {
 	public function index()	{
 		$map = new WorldMap();
 
-		// dd($map->getSize());
-
 		return View::make('world.map.index')
 			->with('map', $map)
 			->with('tiles', $map->getTiles());
