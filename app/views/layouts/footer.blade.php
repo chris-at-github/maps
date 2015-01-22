fc7f165fd1b1e38ffcb5de21baeb30d42113d082
Christian Pschorr <pschorr.christian@gmail.com>
2015-01-22 17:42:25 +0100

    change paths from js and css from relative to absolute

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -1,8 +1,8 @@
 		</div>
 	</div>
 
-	<script type="text/javascript" src="js/jquery-2.1.3.js"></script>
-	<script type="text/javascript" src="js/maps.bundle.js"></script>
+	<script type="text/javascript" src="/js/jquery-2.1.3.js"></script>
+	<script type="text/javascript" src="/js/maps.bundle.js"></script>
 
 	@if(isset($scripts))
 		@foreach ($scripts as $script)
