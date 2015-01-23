9a5cdb13f460bc8c8292836cad7f34d61b207899
Christian Pschorr <pschorr.christian@gmail.com>
2015-01-23 23:29:40 +0100

    change order of versions. highest version is now on top

--- app/views/partials/roadmap.blade.php
+++ app/views/partials/roadmap.blade.php
@@ -1,21 +1,23 @@
 <div class="container container-grey">
 	<ul class="list-roadmap">
 		<li>
-			<header>Version 0.1.0</header>
-			<ul>
-				<li>Erstellung Karte anhand der Übergabe von X,Y über PHP und Speicherung der Daten im Cache</li>
-				<li>Laden der Karte aus dem Cache</li>
-				<li>Weiterleitung auf den Wizard ohne Map -> Auswahl bestehender Karten oder Anlegen einer Neuen</li>
-				<li>Laden der Karte anhand der Session</li>
-			</ul>
-		</li>
-		<li>
 			<header>Version  0.1.1</header>
 			<ul>
 				<li>Einbau Menü mit Punkten World | Services</li>
 				<li>Einbau ServiceController</li>
 				<li>Aufbau Plugin Struktur + Tile Plugins</li>
 				<li>Zuweisung der Tile Typen zu einzelnen Tiles einer Karte und Speicherung der Information</li>
+			</ul>
+		</li>
+		<li>
+			<header>Version 0.1.0</header>
+			<ul>
+				<li class="done">Erstellung Karte über PHP und Speicherung der Daten im Cache</li>
+				<li class="done">Laden der Karte aus dem Cache</li>
+				<li>Weiterleitung auf den Wizard ohne Map -> Auswahl bestehender Karten oder Anlegen einer Neuen</li>
+				<li>Auslagerung der Kartenkonfiguration in ein Partial und dann Einbau in der Wizard</li>
+				<li>Erzeugen der Karte an Hand der X,Y Eingabe</li>
+				<li>Laden der Karte anhand der Session</li>
 			</ul>
 		</li>
 	</ul>
