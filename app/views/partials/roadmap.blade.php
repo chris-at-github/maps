f30b2fda8d9005728f7b249b07f6240c7a3cd024
Christian Pschorr <pschorr.christian@gmail.com>
2015-01-24 00:25:56 +0100

    check some goals as done und add new goals (v0.1.2)

--- app/views/partials/roadmap.blade.php
+++ app/views/partials/roadmap.blade.php
@@ -1,11 +1,19 @@
 <div class="container container-grey">
 	<ul class="list-roadmap">
 		<li>
+			<header>Version  0.1.2</header>
+			<ul>
+				<li>Einbau kleinerer JavaScript Funktionen: Anzeigen Gitter, anzeigen Tile-Position</li>
+				<li>Drag-Drop des Kartenausschnitts</li>
+			</ul>
+		</li>
+		<li>
 			<header>Version  0.1.1</header>
 			<ul>
 				<li>Einbau Menü mit Punkten World | Services</li>
 				<li>Einbau ServiceController</li>
 				<li>Aufbau Plugin Struktur + Tile Plugins</li>
+				<li>Erstellung zweier Tiles -> green, blue</li>
 				<li>Zuweisung der Tile Typen zu einzelnen Tiles einer Karte und Speicherung der Information</li>
 			</ul>
 		</li>
@@ -14,10 +22,11 @@
 			<ul>
 				<li class="done">Erstellung Karte über PHP und Speicherung der Daten im Cache</li>
 				<li class="done">Laden der Karte aus dem Cache</li>
-				<li>Weiterleitung auf den Wizard ohne Map -> Auswahl bestehender Karten oder Anlegen einer Neuen</li>
-				<li>Auslagerung der Kartenkonfiguration in ein Partial und dann Einbau in der Wizard</li>
-				<li>Erzeugen der Karte an Hand der X,Y Eingabe</li>
+				<li class="done">Weiterleitung auf den Wizard ohne Map -> Auswahl bestehender Karten oder Anlegen einer Neuen</li>
+				<li class="done">Auslagerung der Kartenkonfiguration in ein Partial und dann Einbau in der Wizard</li>
+				<li>Erzeugen der Karte an Hand der X,Y Eingabe; Default aus Config</li>
 				<li>Laden der Karte anhand der Session</li>
+				<li>X,Y Feld darf nicht bearbeitet werden -> disabled</li>
 			</ul>
 		</li>
 	</ul>
