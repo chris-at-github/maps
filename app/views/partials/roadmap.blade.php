42d4e8724e0db033ca001591f88eac69aca30b91
Christian Pschorr <pschorr.christian@gmail.com>
2015-01-23 22:10:28 +0100

    Add new goals to roadmap

--- app/views/partials/roadmap.blade.php
+++ app/views/partials/roadmap.blade.php
@@ -1,12 +1,21 @@
 <div class="container container-grey">
 	<ul class="list-roadmap">
 		<li>
-			<header>Version 0.1</header>
+			<header>Version 0.1.0</header>
 			<ul>
 				<li>Erstellung Karte anhand der Übergabe von X,Y über PHP und Speicherung der Daten im Cache</li>
 				<li>Laden der Karte aus dem Cache</li>
 				<li>Weiterleitung auf den Wizard ohne Map -> Auswahl bestehender Karten oder Anlegen einer Neuen</li>
 				<li>Laden der Karte anhand der Session</li>
+			</ul>
+		</li>
+		<li>
+			<header>Version  0.1.1</header>
+			<ul>
+				<li>Einbau Menü mit Punkten World | Services</li>
+				<li>Einbau ServiceController</li>
+				<li>Aufbau Plugin Struktur + Tile Plugins</li>
+				<li>Zuweisung der Tile Typen zu einzelnen Tiles einer Karte und Speicherung der Information</li>
 			</ul>
 		</li>
 	</ul>
