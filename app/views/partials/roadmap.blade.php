af8314a9fea800b4bc5823aaa522e39d533bc301
Christian Pschorr <pschorr.christian@gmail.com>
2015-01-23 07:21:51 +0100

    Init Roadmap

---
+++ app/views/partials/roadmap.blade.php
@@ -0,0 +1,10 @@
+<div class="container">
+	<ul class="list-roadmap">
+		<li>
+			<header>Version 0.1</header>
+			<ul>
+				<li>Erstellung Karte über PHP und Speicherung der Daten im Cache</li>
+			</ul>
+		</li>
+	</ul>
+</div>
