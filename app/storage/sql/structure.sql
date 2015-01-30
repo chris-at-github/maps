bc586ae1a84fcb170b2366288e9e5851b76703dc
Christian Pschorr <c.pschorr@datamints.com>
2015-01-30 14:07:51 +0100

    create plugin structure

--- app/storage/sql/structure.sql
+++ app/storage/sql/structure.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 26. Jan 2015 um 07:41
+-- Erstellungszeit: 30. Jan 2015 um 14:04
 -- Server Version: 5.6.17
 -- PHP-Version: 5.5.12
 
@@ -23,6 +23,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabellenstruktur für Tabelle `plugin`
+--
+
+DROP TABLE IF EXISTS `plugin`;
+CREATE TABLE IF NOT EXISTS `plugin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `namespace` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL,
+  `updated_at` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Tabellenstruktur für Tabelle `world`
 --
 
@@ -33,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `world` (
   `updated_at` timestamp NOT NULL,
   `created_at` timestamp NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
