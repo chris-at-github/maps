f73b4f46890265c9f5c7af2edd920b516b653f99
Christian Pschorr <pschorr.christian@gmail.com>
2015-02-04 06:46:59 +0100

    adjust login container

--- app/views/game/login.blade.php
+++ app/views/game/login.blade.php
@@ -1,16 +1,15 @@
 @include('layouts.header', array('header' => false))
 
+	<div class="vertical-center">
+		<div id="container-login" class="container container-border">
+			{{Form::open(array('route' => 'game.login'))}}
+				@include('partials.user.login')
 
-		<div class="vertical-center">
-			<div id="container-login" class="container">
-				{{Form::open(array('route' => 'game.login'))}}
-					@include('partials.user.login')
-
-					<div class="form-actions">
-						<button type="submit" class="button button-submit">Anmelden</button>
-					</div>
-				{{Form::close()}}
-			</div>
+				<div class="form-actions">
+					<button type="submit" class="button button-submit">Anmelden</button>
+				</div>
+			{{Form::close()}}
 		</div>
+	</div>
 
 @include('layouts.footer')
